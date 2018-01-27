f98de035b04250a158f94acde655231d7d610b99
Jake Passmore <jakecsp@aol.com>
2018-01-27 18:46:55 +0000

    Heron and seagull scripts converted to C#

---
+++ Assets/14734231/Scripts/Seagull/SeagullSoundHeat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeagullSoundHeat {
+
+    public static float heat = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (heat > 0)
+            heat -= Time.deltaTime;
+	}
+}
