4d1477ed0cf08055aa9f92da03bdf71702e68ade
Baker R (FCES) <13194461@glam.ac.uk>
2018-01-29 10:01:09 +0000

    Started converting Heron/Seagull to C#

---
+++ Assets/13194461/Scripts/Heron.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heron : MonoBehaviour {
+
+    float acceleration = 5.00f;
+    float turning = 3.00f;
+
+    float maxIdleTime = 4.00f;
+    float seekPlayerTime = 6.00f;
+    float scaredTime = 4.00f;
+    float fishingTime = 30.00f;
+
+    float shyDistance = 10.00f;
+    float scaredDistance = 5.00f;
+
+    float strechNeckProbability = 10.00f;
+
+    float fishWalkSpeed = 1.00f;
+    float walkSpeed = 1.00f;
+    float runSpeed = 1.00f;
+
+    private HeronStatus status = HeronStatus.Idle;
+
+    private float fishWalkAnimSpeed = 0.50f;
+    private float walkAnimSpeed = 2.00f;
+    private float runAnimSpeed = 9.00f;
+
+    private float minHeight = 34.1f;
+    private float maxHeight = 42.00f;
+
+    enum HeronStatus {Idle = 0, Walking = 1, Running = 2 };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/13194461/Scripts/HeronCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeronCollider : MonoBehaviour {
+
+    float radius = 0.0f;
+    Vector3 position;
+
+    void Awake()
+    {
+        position = transform.position;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color(0.32, 0.55, 0.76, 0.7);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
