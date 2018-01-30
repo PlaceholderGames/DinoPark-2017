1e69a1fdb124a68cb723d2d0d60e992f1e82781d
14065258-YJoshua <scorpjosh@yahoo.com>
2018-01-30 01:22:20 +0000

    Heron c# script
    Completed Heron script. Still needs testing however.

---
+++ Assets/14065258/HeronCollider.cs
@@ -0,0 +1,22 @@
+﻿// Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
+// Do test the code! You usually need to change a few small bits.
+
+using UnityEngine;
+using System.Collections;
+
+public class HeronCollider : MonoBehaviour
+{
+    public float radius = 0.00f;
+    public Vector3 position;
+
+    void Awake()
+    {
+        position = transform.position;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color =  new Color(0.32f, 0.55f, 0.76f, 0.7f);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
