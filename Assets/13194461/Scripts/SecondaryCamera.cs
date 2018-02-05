36d62349e2d03179e06cbf24db305d10b2a4911c
Ryan Baker <13194461@students.southwales.ac.uk>
2018-02-05 00:02:38 +0000

    created a camera that follows a heron you click on

---
+++ Assets/13194461/Scripts/SecondaryCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondaryCamera : MonoBehaviour {
+
+    Vector3 myPos;
+    Vector3 offset;
+    public Transform objPosition;
+    public Camera dinoCam;
+
+	// Use this for initialization
+	void Start () {
+        dinoCam.enabled = false;
+        offset = new Vector3(0.0f, 5.0f, -5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit objHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out objHit))
+            {
+                if (objHit.transform.name == "HeronPrefab")
+                {
+                    objPosition = objHit.transform;
+                    dinoCam.enabled = true;
+                }
+                else
+                {
+                    dinoCam.enabled = false;
+                }
+                  
+                  
+            }
+            else
+            {
+                dinoCam.enabled = false;
+            }
+        }
+
+        transform.position = objPosition.position + myPos + offset;
+	}
+}
