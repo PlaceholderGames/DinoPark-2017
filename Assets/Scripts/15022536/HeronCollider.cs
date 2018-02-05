c11b16e8d01eb308421bec968f6d36b9c21cd3e5
Napiorkowski P (FCES) <15022536@glam.ac.uk>
2018-02-05 09:20:15 +0000

    converting js to c#

---
+++ Assets/Scripts/15022536/HeronCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeronCollider : MonoBehaviour
+{
+
+    public double radius = 0.00;
+    public Vector3 position;
+
+    private void Awake()
+    {
+        position = transform.position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color(0.32, 0.55, 0.76, 0.7);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
