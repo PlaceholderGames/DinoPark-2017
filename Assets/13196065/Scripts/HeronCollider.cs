1d5555f7e5698d8089a5d3011a9e63f32f1f16ea
Connor Page <13196065@students.southwales.ac.uk>
2018-02-07 22:34:16 +0000

    Added heron code

---
+++ Assets/13196065/Scripts/HeronCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeronCollider : MonoBehaviour {
+
+
+    public float radius = 0.0f;
+    public Vector3 position;
+
+    void Awake()
+    {
+        position = transform.position;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0.32f, 0.55f, 0.76f, 0.7f);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
