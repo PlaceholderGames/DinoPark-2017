c48ab5c0a96860bbb7132015e92008b1b404f187
Baker R (FCES) <13194461@glam.ac.uk>
2018-02-12 15:00:23 +0000

    2nd camera is now defaulted as a minimap controlled with the num pad

--- Assets/13194461/Scripts/DinoCam/SecondaryCamera.cs
+++ Assets/13194461/Scripts/DinoCam/SecondaryCamera.cs
@@ -7,6 +7,7 @@ public class SecondaryCamera : MonoBehaviour {
 
     Vector3 myPos;
     Vector3 offset;
+    Vector3 minimapOffset;
     Vector3 centreScreen;
     public Transform objPosition;
     public Transform playerPosition;
@@ -15,21 +16,35 @@ public class SecondaryCamera : MonoBehaviour {
     float y;
     float speed;
     public Image dinoMenu;
+    public Image minimapBackground;
+    bool UseMinimap;
+    float minimapXPos;
+    float minimapYPos;
+    float minimapZPos;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
+        UseMinimap = true;
+        playerPosition = objPosition;
         dinoMenu.enabled = false;
-        dinoCam.enabled = false;
+        dinoCam.enabled = true;
         offset = new Vector3(0.0f, 5.0f, 0.0f);
+        minimapOffset = new Vector3(0.0f, 25.0f, 0.0f);
         x = Screen.width / 2;
         y = Screen.height / 2;
         centreScreen = new Vector3(x, y);
         speed = 3.0f;
-        dinoMenu.rectTransform.sizeDelta = new Vector2(Screen.width * 0.22f, Screen.height * 0.5f);       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        dinoMenu.rectTransform.sizeDelta = new Vector2(Screen.width * 0.22f, Screen.height * 0.5f);
+        minimapBackground.rectTransform.sizeDelta = new Vector2(Screen.width * 0.22f, Screen.height * 0.31f);
+        transform.position = playerPosition.position + minimapOffset;
+        transform.eulerAngles = new Vector3(90.0f, 0.0f, 0.0f);
+        minimapXPos = playerPosition.position.x;
+        minimapYPos = playerPosition.position.y + minimapOffset.y;
+        minimapZPos = playerPosition.position.z;
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit objHit;
@@ -40,25 +55,66 @@ public class SecondaryCamera : MonoBehaviour {
                 if (objHit.transform.name == "HeronPrefab")
                 {
                     objPosition = objHit.transform;
-                    dinoCam.enabled = true;
+                    UseMinimap = false;
                 }
                 else
                 {
-                    dinoCam.enabled = false;
-                }  
+                    UseMinimap = true;
+                    transform.position = playerPosition.position + minimapOffset;
+                    transform.eulerAngles = new Vector3(90.0f, 0.0f, 0.0f);
+                    minimapXPos = playerPosition.position.x;
+                    minimapYPos = playerPosition.position.y + minimapOffset.y;
+                    minimapZPos = playerPosition.position.z;
+                }
             }
             else
             {
-                
-                dinoCam.enabled = false;
+                UseMinimap = true;
             }
         }
 
+        if (UseMinimap == true)
+            Minimap();
+        else
+            FollowCam();
+    }
+
+    void Minimap ()
+    {
+        dinoMenu.enabled = false;
+        minimapBackground.enabled = true;
+
+        if (Input.GetKey("[8]"))
+        {
+            minimapZPos += 0.1f;
+            transform.position = new Vector3(minimapXPos, minimapYPos, minimapZPos);
+        }
+
+        if (Input.GetKey("[2]"))
+        {
+            minimapZPos -= 0.1f;
+            transform.position = new Vector3(minimapXPos, minimapYPos, minimapZPos);
+        }
+
+        if (Input.GetKey("[4]"))
+        {
+            minimapXPos -= 0.1f;
+            transform.position = new Vector3(minimapXPos, minimapYPos, minimapZPos);
+        }
+
+        if (Input.GetKey("[6]"))
+        {
+            minimapXPos += 0.1f;
+            transform.position = new Vector3(minimapXPos, minimapYPos, minimapZPos);
+        }
+
+    }
+
+    void FollowCam()
+    {
         transform.position = objPosition.position - (objPosition.forward * 5) + offset;
         transform.rotation = Quaternion.Lerp(transform.rotation, objPosition.transform.rotation, Time.deltaTime * speed);
-        if (dinoCam.enabled == true)
-            dinoMenu.enabled = true;
-        else
-            dinoMenu.enabled = false;
+        dinoMenu.enabled = true;
+        minimapBackground.enabled = false;
     }
 }
