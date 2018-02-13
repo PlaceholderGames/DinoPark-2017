495ced34b33cda9f415505367aa37e20197c4be1
Jones N (FCES) <14016036@glam.ac.uk>
2018-02-13 13:08:15 +0000

    added code to change between freecam and fpscam, needs fixing still

---
+++ Assets/Standard Assets/Scripts/MoveFPS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFPS : MonoBehaviour {
+
+    public GameObject fpsCamera;
+    public GameObject freeCam;
+    public Transform moveFPS;
+    Camera fpsCam;
+    public FPSFreeCam fpsFreeCam;
+    public Transform mainCamera;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (fpsCamera.activeSelf == true)
+        {
+            if (Input.GetButton("r"))
+            {
+                freeCam.SetActive(true);
+                mainCamera.parent = freeCam.transform;
+                fpsFreeCam.movePosition();
+                fpsCamera.SetActive(false);
+            }
+        }
+    }
+    // Use this for initialization
+    public void movePosition()
+    {
+        mainCamera.transform.position = freeCam.transform.position;
+        moveFPS.transform.position = freeCam.transform.position;
+    }
+}
