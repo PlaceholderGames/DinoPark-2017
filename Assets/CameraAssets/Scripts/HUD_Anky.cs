9e361d1ce2a4b7fd714ffc9f379abba05f983f75
Liam Goodyear <Sporadiclg@hotmail.co.uk>
2018-02-17 13:58:26 +0000

    hud
    Added script to find gameobjects with specific tags

---
+++ Assets/CameraAssets/Scripts/HUD_Anky.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Anky : MonoBehaviour {
+
+	public string dinoTypeTag;
+
+	private GameObject[] dinoList;
+
+	// Use this for initialization
+	void Start () {
+		dinoList = GameObject.FindGameObjectsWithTag (dinoTypeTag);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject x in dinoList) {
+			Debug.Log ( x.name + " Location: " + x.transform.position);
+		}
+	}
+}
