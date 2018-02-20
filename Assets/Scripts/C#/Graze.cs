feb61e02e5554c92eed69a30bded60262d15f011
JSH96 <15014576@students.southwales.ac.uk>
2018-02-20 12:52:20 +0000

    added hungerSO and graze code

--- Assets/Scripts/C#/Graze.cs
+++ Assets/Scripts/C#/Graze.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Graze : MonoBehaviour {
 
+    public Hunger hungerSO;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,15 @@ public class Graze : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        RaycastHit hit;
+        float distance = 100f;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, distance))
+        {
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+            Texture2D tex = rend.material.mainTexture as Texture2D;
+
+            // if tex == grass then eat
+        }
+    }
 }
