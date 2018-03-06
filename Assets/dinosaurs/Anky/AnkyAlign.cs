e46de1fa3e0235ef31587f0395561fe4040d0115
Dr. Mike Reddy <DoctorMikeReddy@gmail.com>
2018-03-06 12:01:35 +0000

    Adding Agency code to Anky (WIP)

---
+++ Assets/dinosaurs/Anky/AnkyAlign.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnkyAlign : AnkyBehaviour
+{
+
+    public float targetRadius;
+    public float slowRadius;
+    public float timeToTarget = 0.1f;
+
+    public override Steering GetSteering()
+    {
+        Steering steering = new Steering();
+        float targetOrientation = target.GetComponent<AnkyAgent>().orientation;
+        float rotation = targetOrientation - agent.orientation;
+        rotation = MapToRange(rotation);
+        float rotationSize = Mathf.Abs(rotation);
+        if (rotationSize < targetRadius)
+            return steering;
+        float targetRotation;
+        if (rotationSize > slowRadius)
+            targetRotation = agent.maxRotation;
+            //targetRotation = maxRotation; //Errata?
+        else
+            targetRotation = agent.maxRotation * rotationSize / slowRadius;
+            //targetRotation = maxRotation * rotationSize / slowRadius; //Errata?
+        targetRotation *= rotation / rotationSize;
+        steering.angular = targetRotation - agent.rotation;
+        steering.angular /= timeToTarget;
+        float angularAccel = Mathf.Abs(steering.angular);
+        if (angularAccel > agent.maxAngularAccel)
+        //if (angularAccel > maxAngularAccel) //Errata?
+        {
+            steering.angular /= angularAccel;
+            steering.angular *= agent.maxAngularAccel;
+            //steering.angular *= maxAngularAccel; // Errata?
+        }
+        return steering;
+    }
+}
---
+++ Assets/dinosaurs/Anky/AnkyFace.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnkyFace : AnkyAlign {
+
+    protected GameObject targetAux;
+
+    public override void Awake()
+    {
+        base.Awake();
+        targetAux = target;
+        target = new GameObject();
+        target.AddComponent<AnkyAgent>();
+    }
+
+    public override Steering GetSteering()
+    {
+        Vector3 direction = targetAux.transform.position - transform.position;
+        if (direction.magnitude > 0.0f)
+        {
+            float targetOrientation = Mathf.Atan2(direction.x, direction.z);
+            targetOrientation *= Mathf.Rad2Deg;
+            target.GetComponent<AnkyAgent>().orientation = targetOrientation;
+        }
+        return base.GetSteering();
+    }
+
+    void OnDestroy ()
+    {
+        Destroy(target);
+    }
+}
