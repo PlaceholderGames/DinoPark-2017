2b290f5df28ad4a5af59602d4ff9e6cf14910a38
Liam Goodyear <Sporadiclg@hotmail.co.uk>
2018-03-23 15:59:42 +0000

    Revert "removed current dead check"
    This reverts commit 7ff6be397958a4763bd0fd455f84a431537aa606.

--- Assets/15017931/Coursework/AI StateMachine/FleeState.cs
+++ Assets/15017931/Coursework/AI StateMachine/FleeState.cs
@@ -42,7 +42,6 @@ public class FleeState : State<MyAnky>
         _owner.setCurrentState(MyAnky.ankyState.FLEEING);
         _owner.fleeBehaviourScript.target = _owner.myTarget.gameObject;
         _owner.fleeBehaviourScript.enabled = true;
-        _owner.wanderBehaviourScript.enabled = false;
         waitForSeconds = 2;
     }
 
@@ -56,7 +55,6 @@ public class FleeState : State<MyAnky>
 
     public override void UpdateState(MyAnky _owner)
     {
-        _owner.wanderBehaviourScript.enabled = false;
         //Check distance between us and our closest hazard
         foreach (Transform pred in _owner.predatorsInRange)
         {
