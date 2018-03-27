4f2d00e7feffe45e44131db4f63def3e0d402cc0
Dr. Mike Reddy <DoctorMikeReddy@gmail.com>
2018-03-27 10:04:48 +0100

    Moved FiannaG code inside Assets folder

---
+++ Assets/14083736/StateAlert.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using StateMachineBase;
+
+
+public class StateAlert : State<MyAnky>
+{
+
+    private static StateAlert _instance;
+
+    private StateAlert()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static StateAlert Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new StateAlert();
+            }
+
+            return _instance;
+        }
+    }
+
+    public override void EnterState(MyAnky _Owner)
+    {
+        Debug.Log("Entering ALERT state");
+        _Owner.anim.SetBool("isAlerted", true);
+        _Owner.anim.SetBool("isFleeing", false);
+    }
+
+    public override void ExitState(MyAnky _Owner)
+    {
+        Debug.Log("Exiting ALERT state");
+        _Owner.anim.SetBool("isAlerted", false);
+
+    }
+
+    public override void UpdateState(MyAnky _Owner)
+    {
+        bool ankyIsSafe = true;
+        foreach (Transform i in _Owner.ankyEnemies)
+        {
+            float Distance = Vector3.Distance(i.position, _Owner.transform.position);
+
+            if (Distance <= 50)
+            {
+                _Owner.ankyFleeing.target = i.gameObject;
+                Debug.Log("Uh oh! The enemies are too close");
+                ankyIsSafe = false;
+;               _Owner.stateMachine.ChangeState(StateFleeing.Instance);
+                
+            }
+        }
+
+        Debug.Log("Finished looping!");
+        if (ankyIsSafe)
+        {
+            Debug.Log("Anky feels safe, no longer alert!");
+            _Owner.stateMachine.ChangeState(StateWalking.Instance);
+            _Owner.ankyEnemies.Clear();
+        }
+    }
+}
---
+++ Assets/14083736/StateDead.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using StateMachineBase;
+
+
+public class StateDead : State<MyAnky>
+{
+
+    private static StateDead _instance;
+
+    private StateDead()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static StateDead Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new StateDead();
+            }
+
+            return _instance;
+        }
+    }
+
+    public override void EnterState(MyAnky _Owner)
+    {
+        Debug.Log("Entering DEAD state");
+        _Owner.anim.SetBool("isDead", true);
+    }
+
+    public override void ExitState(MyAnky _Owner)
+    {
+        Debug.Log("Exiting DEAD state");
+        _Owner.anim.SetBool("isDead", false);
+    }
+
+    public override void UpdateState(MyAnky _Owner)
+    {
+        //Debug.Log("Anky's dead");
+    }
+}
